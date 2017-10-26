70c5c33390cc61a41e4522dff2cddeaef365a711
xiaozhu <810780738@qq.com>
2017-10-26 18:01:14 +0800

    update time tools

--- src/main/java/cn/mywebsite/util/TimeUtil.java
+++ src/main/java/cn/mywebsite/util/TimeUtil.java
@@ -3,14 +3,34 @@ package cn.mywebsite.util;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.aspectj.weaver.patterns.ThisOrTargetAnnotationPointcut;
+
+/**
+ * ClassName:this is time tool class
+ * ClassDescription:
+ * @author zhushouming
+ * @Data 2017年10月26日 下午5:54:35
+ */
 public class TimeUtil {
 	
-	public static String easyTime() {
-		Date date =  new Date();
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String nowTime = dateFormat.format(date);
-		return nowTime;
+	public String customdefault = "yyyy-MM-dd";
+	
+	private final static SimpleDateFormat EASY_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	private final static SimpleDateFormat CHINA_DATE_FORMAT = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+	
+	
+	public final static String easyTime() {
+		return EASY_DATE_FORMAT.format(new Date());
 	}
 	
 	
+	public final static String chinaTime(){
+		return CHINA_DATE_FORMAT.format(new Date());
+	}
+	
+	
+	public static String customTime(String customdefault){
+		this.customdefault = customdefault;
+	}
+
 }
