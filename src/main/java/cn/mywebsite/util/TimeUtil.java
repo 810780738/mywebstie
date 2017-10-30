fd93156df6f97850fff10cf8f209d17bb9320736
xiaozhu <810780738@qq.com>
2017-10-30 16:08:16 +0800

    optimize time calss

--- src/main/java/cn/mywebsite/util/TimeUtil.java
+++ src/main/java/cn/mywebsite/util/TimeUtil.java
@@ -1,9 +1,8 @@
 package cn.mywebsite.util;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-
-import org.aspectj.weaver.patterns.ThisOrTargetAnnotationPointcut;
 
 /**
  * ClassName:this is time tool class
@@ -13,24 +12,35 @@ import org.aspectj.weaver.patterns.ThisOrTargetAnnotationPointcut;
  */
 public class TimeUtil {
 	
-	public String customdefault = "yyyy-MM-dd";
+	public static final String customdefault = "yyyy-MM-dd";
 	
 	private final static SimpleDateFormat EASY_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	private final static SimpleDateFormat CHINA_DATE_FORMAT = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
 	
 	
 	public final static String easyTime() {
-		return EASY_DATE_FORMAT.format(new Date());
+		return EASY_DATE_FORMAT.format(Calendar.getInstance().getTime());
 	}
 	
 	
 	public final static String chinaTime(){
-		return CHINA_DATE_FORMAT.format(new Date());
+		return CHINA_DATE_FORMAT.format(Calendar.getInstance().getTime());
 	}
 	
 	
 	public static String customTime(String customdefault){
-		this.customdefault = customdefault;
+//		this.customdefault = customdefault;
+		return null;
 	}
-
+	
+	
+	public static int generateDateDay(Date date){
+		Calendar cal = Calendar.getInstance();
+		if (date == null)date = cal.getTime();
+		cal.setTime(date);
+		return cal.get(Calendar.YEAR) * 10000 
+				+ ((cal.get(Calendar.MONTH) + 1) * 100) 
+				+ cal.get(Calendar.DAY_OF_MONTH);
+	}
+	
 }
