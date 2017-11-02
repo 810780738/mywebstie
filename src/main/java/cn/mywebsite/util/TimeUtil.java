72487ca64db0952b11e1e8d8c199e14a1697dc51
xiaozhu <810780738@qq.com>
2017-11-02 11:11:50 +0800

    don'tknow

--- src/main/java/cn/mywebsite/util/TimeUtil.java
+++ src/main/java/cn/mywebsite/util/TimeUtil.java
@@ -1,8 +1,13 @@
 package cn.mywebsite.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 
 /**
  * ClassName:this is time tool class
@@ -11,6 +16,8 @@ import java.util.Date;
  * @Data 2017年10月26日 下午5:54:35
  */
 public class TimeUtil {
+	
+	public static Log logger = LogFactory.getLog(TimeUtil.class);
 	
 	public static final String customdefault = "yyyy-MM-dd";
 	
@@ -41,6 +48,21 @@ public class TimeUtil {
 		return cal.get(Calendar.YEAR) * 10000 
 				+ ((cal.get(Calendar.MONTH) + 1) * 100) 
 				+ cal.get(Calendar.DAY_OF_MONTH);
+	}
+	
+	public static Date convertToDate(String pattern, Date defValue){
+		try {
+			return EASY_DATE_FORMAT.parse(pattern);
+		} catch (ParseException e) {
+			logger.warn(String.format("无法将[%s]字符串转化成日期格式，异常信息：%s", pattern, e.getMessage()));
+			return defValue;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Date convertToDate = convertToDate("2017-10-23 14:48:09.0",Calendar.getInstance().getTime());
+		String format = EASY_DATE_FORMAT.format(convertToDate);
+		System.out.println(format);
 	}
 	
 }
